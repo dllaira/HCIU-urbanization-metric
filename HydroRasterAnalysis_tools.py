e65f21aaf92b3a78cc1384c864f469c8ffbb0100
dllaira <76715244+dllaira@users.noreply.github.com>
2024-12-13 16:35:28 -0600

    HydroRasterAnalysis_tools.py

--- HydroRasterAnalysis_tools.py
+++ HydroRasterAnalysis_tools.py
@@ -9,9 +9,9 @@ Suite of functions for performing traditional DEM hydrologic analysis operations
 using existing open-source Python software, or implementing literature algorithms. 
 
 These functions represent an asset associated with the research work by 
-F. Dell'Aira and C. I. Meier (2024), titled: "Beyond Total Impervious Area: 
+F. Dell'Aira and C. I. Meier (2024). "Beyond Total Impervious Area: 
 A New Lumped Descriptor of Basin-Wide Hydrologic Connectivity for 
-Characterizing Urbanizing Watersheds".
+Characterizing Urban Watersheds". Hydrology and Earth System Sciences.
 
 DEPENDENCIES: 
 1) Numpy Python library (https://numpy.org/)
